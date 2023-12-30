2cd4de5a482dd3f6ba1ab5eb34c230713ad3228f
Aarif S <aarifcad01@brightskills.com>
2023-12-30 15:36:04 +0530

    add files

---
+++ JS/script.js
@@ -0,0 +1,37 @@
+function addition(){
+
+    var a=10;
+    var b=40;
+    var c=a+b;
+
+
+console.log ("added value a and b is ..."+c);
+console.log (a+b);
+console.log (a-b);
+console.log (a*b)
+console.log (a/b)
+console.log (a%b)
+console.log (b=a);
+console.log (a+=b);
+console.log (a-=b)
+console.log (b);
+console.log (a*=b)
+console.log (a/=b)
+console.log (a%=b)
+console.log (a**=b)
+
+console.log (a==b)
+console.log (a===b)
+
+console.log (a*b)
+console.log (value )
+}
+addition();
+
+document.getElementById("fnname").innerHTML=document.getElementById("fname").value;
+document.getElementById("lnname").innerHTML=document.getElementById("lname").value;
+document.getElementById("pphone number").innerHTML=document.getElementById("phone number").value;
+function name() {
+    let specs
+    
+}
